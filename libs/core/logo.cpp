1e142eeaf150cb452742e93b1d5a87ed1efb01d6
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-10-22 01:08:55 -0700

    Logo released (#3461)
    * add on-logo-released in advacnced
    
    * add help links
    
    * adding title on circle
    
    * more titles
    
    * theme updates
    
    Co-authored-by: Peli de Halleux <peli@Pelis-MacBook-Pro.local>

--- libs/core/logo.cpp
+++ libs/core/logo.cpp
@@ -9,6 +9,7 @@ namespace input {
     //% blockId=input_logo_event block="on logo pressed"
     //% group="micro:bit v2"
     //% parts="logotouch"
+    //% help="input/on-logo-pressed"
     void onLogoPressed(Action body) {
 #if MICROBIT_CODAL
         registerWithDal(uBit.logo.id, MICROBIT_BUTTON_EVT_CLICK, body);
@@ -17,6 +18,23 @@ namespace input {
 #endif
     }
 
+    /**
+     * Do something when the logo is released.
+     * @param body the code to run when the logo is released
+     */
+    //% weight=6 blockGap=16
+    //% blockId=input_logo_released block="on logo released"
+    //% advanced=true
+    //% group="micro:bit v2"
+    //% parts="logotouch"
+    //% help="input/on-logo-released"
+    void onLogoReleased(Action body) {
+#if MICROBIT_CODAL
+        registerWithDal(uBit.logo.id, MICROBIT_BUTTON_EVT_UP, body);
+#else
+        target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+#endif
+    }
 
     /**
      * Get the logo state (pressed or not).
@@ -26,6 +44,7 @@ namespace input {
     //% blockGap=8
     //% group="micro:bit v2"
     //% parts="logotouch"
+    //% help="input/is-logo-pressed"
     bool logoIsPressed() {
 #if MICROBIT_CODAL
         return uBit.io.logo.isTouched();
