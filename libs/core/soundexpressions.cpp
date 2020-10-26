f7f47d468e6ff371efff63043dbfba297129f334
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-10-26 12:37:15 -0700

    Sound expressions (#3486)
    * basic sound epxression support
    
    * hooking up expressions
    
    * sim shims
    
    * updated sim code
    
    * silly typo

---
+++ libs/core/soundexpressions.cpp
@@ -0,0 +1,18 @@
+#include "pxt.h"
+
+namespace music {
+    /**
+     * Internal use only
+     **/
+    //% async
+    void __playSoundExpression(String nodes, bool waitTillDone) {
+#if MICROBIT_CODAL
+        if (waitTillDone)
+            uBit.audio.soundExpressions.play(MSTR(nodes));
+        else
+            uBit.audio.soundExpressions.playAsync(MSTR(nodes));
+#else
+        target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+#endif
+    }
+}
