28f0f617f8f55ecdb10cbca52409b32de584312d
Michal Moskal <michal@moskal.me>
2020-10-29 16:34:00 +0100

    Move microphone init here from codal

--- libs/microphone/microphonehw.cpp
+++ libs/microphone/microphonehw.cpp
@@ -4,46 +4,27 @@
 #include "pxt.h"
 
 #if MICROBIT_CODAL
+
 #include "LevelDetector.h"
 #include "LevelDetectorSPL.h"
 #include "DataStream.h"
 
-#ifndef MIC_DEVICE
-// STM?
-class DummyDataSource : public codal::DataSource {
-  public:
-    DummyDataSource() {}
-};
-class PanicPDM {
-  public:
-    uint8_t level;
-    DummyDataSource source;
-    codal::DataStream output;
-
-    PanicPDM(Pin &sd, Pin &sck) : output(source) { target_panic(PANIC_MICROPHONE_MISSING); }
-    void enable() {}
-    void disable() {}
-};
-#define MIC_DEVICE PanicPDM
-#endif
-
-#ifndef MIC_INIT
-#define MIC_INIT                                                                                   \
-        : microphone(*LOOKUP_PIN(MIC_DATA), *LOOKUP_PIN(MIC_CLOCK)) \
-        , level(microphone.output, 95.0, 75.0, 9, 52, DEVICE_ID_MICROPHONE)
-#endif
-
-#ifndef MIC_ENABLE
-#define MIC_ENABLE microphone.enable()
-#endif
 
 namespace pxt {
 
 class WMicrophone {
   public:
-    MIC_DEVICE microphone;
+    NRF52ADCChannel *microphone;
+    StreamNormalizer normalizer;
     LevelDetectorSPL level;
-    WMicrophone() MIC_INIT { MIC_ENABLE; }
+    WMicrophone()
+        : microphone(uBit.adc.getChannel(uBit.io.microphone)),
+          normalizer(microphone->output, 1.0f, true, DATASTREAM_FORMAT_UNKNOWN, 10),
+          level(normalizer.output, 75.0, 60.0, 9, 52, DEVICE_ID_MICROPHONE) {
+        uBit.io.runmic.setDigitalValue(1);
+        uBit.io.runmic.setHighDrive(true);
+        microphone->setGain(7, 0);
+    }
 };
 SINGLETON(WMicrophone);
 
