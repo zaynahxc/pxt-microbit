f2761982ee3f9fc23fafd67e0ddb625c39bfbfce
Michal Moskal <michal@moskal.me>
2020-10-29 17:34:44 +0100

    Add input.onSoundData()

---
+++ libs/microphone/micbuffer.ts
@@ -0,0 +1,19 @@
+namespace input {
+    //% shim=microphone::_readyEvent
+    function _readyEvent(): number {
+        return 0xffff
+    }
+
+    //% shim=microphone::_pull
+    function _pull(): Buffer {
+        return null
+    }
+
+    export function onSoundData(cb: (data: Buffer) => void) {
+        control.internalOnEvent(DAL.DEVICE_ID_NOTIFY, _readyEvent(), () => {
+            const buf = _pull()
+            if (buf)
+                cb(buf)
+        })
+    }
+}
--- libs/microphone/microphonehw.cpp
+++ libs/microphone/microphonehw.cpp
@@ -9,21 +9,57 @@
 #include "LevelDetectorSPL.h"
 #include "DataStream.h"
 
-
 namespace pxt {
+
+class TeeStream : public DataSink, public DataSource {
+  public:
+    int evid;
+    DataSource &upstream;
+    DataStream output;
+    ManagedBuffer buffer;
+    bool pxtPending;
+
+    TeeStream(int evid, DataSource &src) : evid(evid), upstream(src), output(*this) {
+        pxtPending = false;
+        output.setBlocking(false);
+        upstream.connect(*this);
+    }
+
+    Buffer pxtBuffer() {
+        if (!pxtPending)
+            return nullptr;
+        pxtPending = false;
+        return mkBuffer(buffer.getBytes(), buffer.length());
+    }
+
+    virtual ManagedBuffer pull() override { return buffer; }
+    virtual int getFormat() override { return upstream.getFormat(); }
+    virtual int setFormat(int format) override { return upstream.setFormat(format); }
+
+    virtual int pullRequest() override {
+        buffer = upstream.pull();
+        pxtPending = buffer.length() > 0;
+        output.pullRequest();
+        Event(DEVICE_ID_NOTIFY, evid);
+        return DEVICE_OK;
+    }
+};
 
 class WMicrophone {
   public:
     NRF52ADCChannel *microphone;
     StreamNormalizer normalizer;
+    TeeStream tee;
     LevelDetectorSPL level;
     WMicrophone()
         : microphone(uBit.adc.getChannel(uBit.io.microphone)),
           normalizer(microphone->output, 1.0f, true, DATASTREAM_FORMAT_UNKNOWN, 10),
-          level(normalizer.output, 75.0, 60.0, 9, 52, DEVICE_ID_MICROPHONE) {
+          tee(allocateNotifyEvent(), normalizer.output),
+          level(tee.output, 75.0, 60.0, 9, 52, DEVICE_ID_MICROPHONE) {
         uBit.io.runmic.setDigitalValue(1);
         uBit.io.runmic.setHighDrive(true);
         microphone->setGain(7, 0);
+        DMESG("microphone: %d Hz", 1000000 / uBit.adc.getSamplePeriod());
     }
 };
 SINGLETON(WMicrophone);
@@ -34,4 +70,29 @@ codal::LevelDetectorSPL *getMicrophoneLevel() {
 }
 
 } // namespace pxt
+
 #endif
+
+namespace microphone {
+
+//%
+int _readyEvent() {
+#if MICROBIT_CODAL
+    return getWMicrophone()->tee.evid;
+#else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+    return 0;
+#endif
+}
+
+//%
+Buffer _pull() {
+#if MICROBIT_CODAL
+    return getWMicrophone()->tee.pxtBuffer();
+#else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+    return nullptr;
+#endif
+}
+
+} // namespace microphone
