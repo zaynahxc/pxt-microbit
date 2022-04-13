25b3fe1f91453dfafd389a029b539f364f4ff2d0
Richard Knoll <riknoll@users.noreply.github.com>
2022-04-13 14:18:23 -0700

    Add sound effect blocks (#4506)
    * Add sound expression block
    
    * remove errant space

--- libs/core/soundexpressions.ts
+++ libs/core/soundexpressions.ts
@@ -35,6 +35,28 @@ class SoundExpression {
     }
 }
 
+enum WaveShape {
+    Sine = 0,
+    Sawtooth = 1,
+    Triangle = 2,
+    Square = 3,
+    Noise = 4
+}
+
+enum InterpolationCurve {
+    None,
+    Linear,
+    Curve,
+    Logarithmic
+}
+
+enum SoundExpressionEffect {
+    None = 0,
+    Vibrato = 1,
+    Tremolo = 2,
+    Warble = 3
+}
+
 namespace soundExpression {
     //% fixedInstance whenUsed block="{id:soundexpression}giggle"
     export const giggle = new SoundExpression("giggle");
@@ -56,4 +78,278 @@ namespace soundExpression {
     export const twinkle = new SoundExpression("twinkle");
     //% fixedInstance whenUsed block="{id:soundexpression}yawn"
     export const yawn = new SoundExpression("yawn");
+
+    export enum InterpolationEffect {
+        None = 0,
+        Linear = 1,
+        Curve = 2,
+        ExponentialRising = 5,
+        ExponentialFalling = 6,
+        ArpeggioRisingMajor = 8,
+        ArpeggioRisingMinor = 10,
+        ArpeggioRisingDiminished = 12,
+        ArpeggioRisingChromatic = 14,
+        ArpeggioRisingWholeTone = 16,
+        ArpeggioFallingMajor = 9,
+        ArpeggioFallingMinor = 11,
+        ArpeggioFallingDiminished = 13,
+        ArpeggioFallingChromatic = 15,
+        ArpeggioFallingWholeTone = 17,
+        Logarithmic = 18
+    }
+
+    export class Sound {
+        src: string;
+
+        constructor() {
+            this.src = "000000000000000000000000000000000000000000000000000000000000000000000000"
+        }
+
+        get wave(): WaveShape {
+            return this.getValue(0, 1);
+        }
+
+        set wave(value: WaveShape) {
+            this.setValue(0, Math.constrain(value, 0, 4), 1);
+        }
+
+        get volume() {
+            return this.getValue(1, 4);
+        }
+
+        set volume(value: number) {
+            this.setValue(1, Math.constrain(value, 0, 1023), 4);
+        }
+
+        get frequency() {
+            return this.getValue(5, 4);
+        }
+
+        set frequency(value: number) {
+            this.setValue(5, value, 4);
+        }
+
+        get duration() {
+            return this.getValue(9, 4);
+        }
+
+        set duration(value: number) {
+            this.setValue(9, value, 4);
+        }
+
+        get shape(): InterpolationEffect {
+            return this.getValue(13, 2);
+        }
+
+        set shape(value: InterpolationEffect) {
+            this.setValue(13, value, 2);
+        }
+
+        get endFrequency() {
+            return this.getValue(18, 4);
+        }
+
+        set endFrequency(value: number) {
+            this.setValue(18, value, 4);
+        }
+
+        get endVolume() {
+            return this.getValue(26, 4);
+        }
+
+        set endVolume(value: number) {
+            this.setValue(26, Math.constrain(value, 0, 1023), 4);
+        }
+
+        get steps() {
+            return this.getValue(30, 4);
+        }
+
+        set steps(value: number) {
+            this.setValue(30, value, 4);
+        }
+
+        get fx(): SoundExpressionEffect {
+            return this.getValue(34, 2);
+        }
+
+        set fx(value: SoundExpressionEffect) {
+            this.setValue(34, Math.constrain(value, 0, 3), 2);
+        }
+
+        get fxParam() {
+            return this.getValue(36, 4);
+        }
+
+        set fxParam(value: number) {
+            this.setValue(36, value, 4);
+        }
+
+        get fxnSteps() {
+            return this.getValue(40, 4);
+        }
+
+        set fxnSteps(value: number) {
+            this.setValue(40, value, 4);
+        }
+
+        get frequencyRandomness() {
+            return this.getValue(44, 4);
+        }
+
+        set frequencyRandomness(value: number) {
+            this.setValue(44, value, 4);
+        }
+
+        get endFrequencyRandomness() {
+            return this.getValue(48, 4);
+        }
+
+        set endFrequencyRandomness(value: number) {
+            this.setValue(48, value, 4);
+        }
+
+        get volumeRandomness() {
+            return this.getValue(52, 4);
+        }
+
+        set volumeRandomness(value: number) {
+            this.setValue(52, value, 4);
+        }
+
+        get endVolumeRandomness() {
+            return this.getValue(56, 4);
+        }
+
+        set endVolumeRandomness(value: number) {
+            this.setValue(56, value, 4);
+        }
+
+        get durationRandomness() {
+            return this.getValue(60, 4);
+        }
+
+        set durationRandomness(value: number) {
+            this.setValue(60, value, 4);
+        }
+
+        get fxParamRandomness() {
+            return this.getValue(64, 4);
+        }
+
+        set fxParamRandomness(value: number) {
+            this.setValue(64, value, 4);
+        }
+
+        get fxnStepsRandomness() {
+            return this.getValue(68, 4);
+        }
+
+        set fxnStepsRandomness(value: number) {
+            this.setValue(68, value, 4);
+        }
+
+        copy() {
+            const result = new Sound();
+            result.src = this.src.slice(0);
+            return result;
+        }
+
+        protected setValue(offset: number, value: number, length: number) {
+            value = Math.constrain(value | 0, 0, Math.pow(10, length) - 1);
+            this.src = this.src.substr(0, offset) + formatNumber(value, length) + this.src.substr(offset + length);
+        }
+
+        protected getValue(offset: number, length: number) {
+            return parseInt(this.src.substr(offset, length));
+        }
+    }
+
+    function formatNumber(num: number, length: number) {
+        let result = num + "";
+        while (result.length < length) result = "0" + result;
+        return result;
+    }
+
+    export function playSound(toPlay: Sound | Sound[]) {
+        let src = "";
+        if (Array.isArray(toPlay)) {
+            src = (toPlay as Sound[]).map(s => s.src).join(",");
+        }
+        else {
+            src = (toPlay as Sound).src;
+        }
+
+        new SoundExpression(src).playUntilDone();
+    }
+
+    //% blockId=soundExpression_playSoundEffect
+    //% blockNamespace=music
+    //% block="play sound $sound"
+    //% sound.shadow=soundExpression_createSoundEffect
+    //% weight=101
+    export function playSoundEffect(sound: soundExpression.Sound) {
+        soundExpression.playSound(sound);
+    }
+
+    //% blockId=soundExpression_createSoundEffect
+    //% blockNamespace=music
+    //% block="$waveShape|| start frequency $startFrequency end fequency $endFrequency duration $duration start volume $startVolume end volume $endVolume effect $effect interpolation $interpolation"
+    //% waveShape.defl=WaveShape.Sine
+    //% waveShape.fieldEditor=soundeffect
+    //% startFrequency.defl=2000
+    //% endFrequency.defl=0
+    //% startVolume.defl=1023
+    //% endVolume.defl=0
+    //% duration.defl=500
+    //% effect.defl=SoundExpressionEffect.None
+    //% interpolation.defl=InterpolationCurve.Linear
+    //% compileHiddenArguments=true
+    //% inlineInputMode="variable"
+    export function createSoundEffect(waveShape: WaveShape, startFrequency: number, endFrequency: number, startVolume: number, endVolume: number, duration: number, effect: SoundExpressionEffect, interpolation: InterpolationCurve) {
+        const sound = new soundExpression.Sound();
+        sound.wave = waveShape;
+        sound.frequency = startFrequency;
+        sound.volume = startVolume;
+        sound.endFrequency = endFrequency;
+        sound.endVolume = endVolume;
+        sound.duration = duration;
+        sound.fx = effect;
+
+        switch (interpolation) {
+            case InterpolationCurve.None:
+                sound.shape = soundExpression.InterpolationEffect.None;
+                sound.steps = 128;
+                break;
+            case InterpolationCurve.Linear:
+                sound.shape = soundExpression.InterpolationEffect.Linear;
+                sound.steps = 128;
+                break;
+            case InterpolationCurve.Curve:
+                sound.shape = soundExpression.InterpolationEffect.Curve;
+                sound.steps = 90;
+                break;
+            case InterpolationCurve.Logarithmic:
+                sound.shape = soundExpression.InterpolationEffect.Logarithmic;
+                sound.steps = 90;
+                break;
+        }
+
+        switch (sound.fx) {
+            case SoundExpressionEffect.Vibrato:
+                sound.fxnSteps = 512;
+                sound.fxParam = 2;
+                break;
+            case SoundExpressionEffect.Tremolo:
+                sound.fxnSteps = 900;
+                sound.fxParam = 3;
+                break;
+            case SoundExpressionEffect.Warble:
+                sound.fxnSteps = 700;
+                sound.fxParam = 2;
+                break;
+        }
+
+        return sound;
+    }
 }
