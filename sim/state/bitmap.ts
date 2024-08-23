d37d7de98a46aa731dfba975f5a6b77f1b3d61dc
Tom Ball <tball@microsoft.com>
2024-08-22 18:38:09 -0700

    move helpers

--- sim/state/bitmap.ts
+++ sim/state/bitmap.ts
@@ -1105,3 +1105,27 @@ namespace pxsim.bitmap {
         return toBuffer(img)
     }
 }
+
+namespace pxsim.pxtcore {
+    export function updateScreen(img: RefImage) {
+
+    }
+    export function updateStats(s: string) {
+
+    }
+    export function setPalette(b: RefBuffer) {
+
+    }
+    export function setScreenBrightness(b: number) {
+   
+    }
+    export function displayHeight(): number {
+        return 120
+    }
+    export function displayWidth(): number {
+        return 160
+    }
+    export function displayPresent(): boolean {
+        return true
+    }
+}
