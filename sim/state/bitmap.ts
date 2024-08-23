86a7dcf91bc118a890ab347f4a14d652ef6da487
Tom Ball <tball@microsoft.com>
2024-08-23 13:56:38 -0700

    split out screen simulator shims

--- sim/state/bitmap.ts
+++ sim/state/bitmap.ts
@@ -1109,27 +1109,3 @@ namespace pxsim.bitmap {
         return toBuffer(img)
     }
 }
-
-namespace pxsim.pxtcore {
-    export function updateScreen(img: RefImage) {
-
-    }
-    export function updateStats(s: string) {
-
-    }
-    export function setPalette(b: RefBuffer) {
-
-    }
-    export function setScreenBrightness(b: number) {
-   
-    }
-    export function displayHeight(): number {
-        return 120
-    }
-    export function displayWidth(): number {
-        return 160
-    }
-    export function displayPresent(): boolean {
-        return true
-    }
-}
